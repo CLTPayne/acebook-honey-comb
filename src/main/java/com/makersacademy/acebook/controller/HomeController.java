35daa6686b1ccce2b53b767643be6f7f2155aa8f
Khie Biggs <khiebigzy@gmail.com>
2018-07-05 16:44:43 +0100

    add create post functionality

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -2,13 +2,12 @@ package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.makersacademy.acebook.model.Post;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HomeController {
@@ -25,4 +24,18 @@ public class HomeController {
 	public List<Post> otherIndex(){
 		return postRepository.findAll();
 	}
+
+	@PostMapping("/posts")
+	public Post create(@RequestBody Map<String, String> body){
+		String content = body.get("content");
+		return postRepository.save(new Post(content));
+	}
+
+//	@PutMapping("/post/{id}")
+//	public Post update(@PathVariable String id, @RequestBody Map<String, String> content){
+//		int postId = Integer.parseInt(id);
+//		Post post = postRepository.findOne(postId);
+//		post.setContent(content.get("content"));
+//		return postRepository.save(post);
+//	}
 }
--- src/main/java/com/makersacademy/acebook/repository/PostRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/PostRepository.java
@@ -4,6 +4,8 @@ import com.makersacademy.acebook.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
