ee720f983b189d1225e05090ef7cd08241f6e7d1
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-06 18:46:57 +0100

    Change database to PSQL for persistent data
    * Add user model and table

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -2,14 +2,12 @@ package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.makersacademy.acebook.model.Post;
 
-import java.util.List;
-import java.util.Map;
-
-@RestController
+@Controller
 public class HomeController {
 
 	@RequestMapping(value = "/")
@@ -17,25 +15,5 @@ public class HomeController {
 		return "index";
 	}
 
-	@Autowired
-	PostRepository postRepository;
 
-	@GetMapping("/posts")
-	public List<Post> otherIndex(){
-		return postRepository.findAll();
-	}
-
-	@PostMapping("/posts")
-	public Post create(@RequestBody Map<String, String> body){
-		String content = body.get("content");
-		return postRepository.save(new Post(content));
-	}
-
-//	@PutMapping("/post/{id}")
-//	public Post update(@PathVariable String id, @RequestBody Map<String, String> content){
-//		int postId = Integer.parseInt(id);
-//		Post post = postRepository.findOne(postId);
-//		post.setContent(content.get("content"));
-//		return postRepository.save(post);
-//	}
 }
