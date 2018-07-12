b1e0444bc3c92fdfaa6f633959f0d6db6045ebc0
Khie Biggs <khiebigzy@gmail.com>
2018-07-12 14:37:41 +0100

    changed the type of object returns by new post routing

--- src/main/java/com/makersacademy/acebook/controller/PostController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostController.java
@@ -4,7 +4,10 @@ import com.makersacademy.acebook.model.Authenticator;
 import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.repository.PostRepository;
 import com.makersacademy.acebook.repository.UserRepository;
+import main.java.com.makersacademy.acebook.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,9 +29,9 @@ public class PostController {
         String token = body.get("token");
         Long userId  = Authenticator.returnIdFromToken(userRepository, token);
             if(userId == null){
-                return "user could not be matched to a token";
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new Response("user could not be matched to a token"));
             }
-        return postRepository.save(new Post(content, userId));
+        return ResponseEntity.status(HttpStatus.OK).body(postRepository.save(new Post(content, userId)));
     }
 
     @GetMapping("/posts")
