6f5eb16776f6d613e806669f8b38e80ec2fb935b
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-13 10:24:59 +0100

    Update sending of a post from front end interface
    * Previously the ajax request was missing the token field so posting
    failed. Hardcoded in token into addpost.js to  complete a successful
    HTTP POST via the API.

--- src/main/js/posts/addpost.js
+++ src/main/js/posts/addpost.js
@@ -4,8 +4,9 @@ import client from "../client";
 class AddPost extends React.Component {
     constructor(props) {
         super(props);
-        this.state ={
-            content: ""
+        this.state = {
+            content: "",
+            token: ""
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -22,8 +23,10 @@ class AddPost extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
         let data = {
-                  content : this.state.content
-        }
+                  content : this.state.content,
+                  token : "$2a$10$GBSG3lRRYRpfFWz4FP8SgOcVLdcU2h85rVSBIBEmsL19CVOa/dlLi"
+                  }
+
         $.ajax({
                   url: "http://localhost:8080/posts",
                   type: "POST",
